f010bc907465c70d003fa77a9e0657a1958e48f0
James Wiesenthal <jameswiesenthal@mail.mccneb.edu>
2022-10-03 17:44:06 -0600

    change sizing in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -10,10 +10,10 @@ const Footer = () => {
             <div className="footer-container">
                 <p>Engagement and Wedding Photography by</p>
                 <a href="https://www.madisonmichellephotography.com/" target="_blank">
-                    <img width="250" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/MadisonLogo.png?alt=media&token=550b96c0-425f-4699-87c0-ba77d28793bd" />
+                    <img width="170" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/MadisonLogo.png?alt=media&token=550b96c0-425f-4699-87c0-ba77d28793bd" />
                 </a>
                 <hr></hr>
-                <p>Website Development and Design by yours truly, Jake</p>
+                <p>Website Development and Design by yours&nbsp;truly, Jake</p>
             </div>
         </>
     )
