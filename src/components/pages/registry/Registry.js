25d44831d4d2ea3dea34505ac2f0c68f81ab0889
James Wiesenthal <jameswiesenthal@mail.mccneb.edu>
2022-11-20 11:54:40 -0700

    fixed mobile styles for registry page

--- src/components/pages/registry/Registry.js
+++ src/components/pages/registry/Registry.js
@@ -3,7 +3,6 @@ import React from 'react';
 const Registry = () => {
 
     function openRegistryLink(url) {
-        console.log(url);
         window.open(url);
     }
 
@@ -13,7 +12,7 @@ const Registry = () => {
             <div className="registry-flexbox-container">
                 {/* Target */}
                 <div className="registry-flexbox-item" onClick={() => openRegistryLink("https://www.target.com/gift-registry/gift/megjake")}>
-                    <img height="100px" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/logos%2FTarget_Bullseye-Logo_Red_transparent.png?alt=media&token=6b75f3fd-b333-4e92-bead-24a44bfd513a" className="registry-logo" />
+                    <img height="100px" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/logos%2FTarget_Bullseye-Logo_Red_transparent-min.png?alt=media&token=3210db59-536d-48d5-995a-85242466b36c" className="registry-logo" />
                     <p className="registry-link">Target</p>
                 </div>
                 {/* Bed Bath & Beyond */}
@@ -26,12 +25,12 @@ const Registry = () => {
             <div className="registry-flexbox-container">
                 {/* Nebraska Furniture Mart */}
                 <div className="registry-flexbox-item" onClick={() => openRegistryLink("https://www.nfm.com/giftcards")}>
-                    <img height="100px" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/logos%2FNebraska_Furniture_Mart_logo.png?alt=media&token=d446f69c-bcbe-4d7b-8f00-ab8a24952359" className="registry-logo" />
+                    <img height="100px" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/logos%2FNebraska_Furniture_Mart_logo-min.png?alt=media&token=d892438b-9be6-4fbe-aa9b-8dfc237d9d4f" className="registry-logo" />
                     <p className="registry-link">Nebraska Furniture Mart</p>
                 </div>
                 {/* Lowe's */}
                 <div className="registry-flexbox-item" onClick={() => openRegistryLink("https://www.lowes.com/l/shop/gift-cards")}>
-                    <img height="100px" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/logos%2FLowes_logo_pms_280.png?alt=media&token=0e86e028-62a8-4c9a-98b4-e3251802929c" className="registry-logo" />
+                    <img height="100px" src="https://firebasestorage.googleapis.com/v0/b/jakeandmeg.appspot.com/o/logos%2FLowes_logo_pms_280-min.png?alt=media&token=d23f87a1-f281-4630-a7d3-ec1e35bada5a" className="registry-logo" />
                     <p className="registry-link">Lowe's</p>
                 </div>
             </div>
---
+++ src/components/pages/things-to-do/ThingsToDo.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const ThingsToDo = () => {
+
+    return (
+        <div className="things-container">
+            <h1>Some of Meg and Jake's Favorite Places in Omaha</h1>
+            <div className="things-food-container">
+                <div className="things-item">
+                    
+                </div>
+            </div>
+            <div className="things-places-container">
+
+            </div>
+        </div>
+    )
+}
+
+export default ThingsToDo
